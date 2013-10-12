aadaa331bb12fdc45ef512b74562371293196000
josh1zn <josh1zn@gmail.com>
2013-10-12 17:49:50 +0200

    Project Base complete and ready for work

---
+++ DVDRentalCode/RentalItemBLL.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DVDRentalData;
+
+namespace DVDRentalCode
+{
+    public class RentalItemBLL
+    {
+        public List<RentalItemDto> GetAllRentalItems()
+        {
+            DVDRentalEntities db = new DVDRentalEntities();
+            List<RentalItemDto> LRI = new List<RentalItemDto>();
+            foreach (var ri in db.RentalItems)
+            {
+                LRI.Add(new RentalItemDto
+                {
+                    ID = ri.ID.ToString(),
+                    Name = ri.Name,
+                    Type = ri.Type,
+                    Price = ri.Price.ToString()
+                });
+            }
+            return LRI;
+        }
+    }
+
+    public class RentalItemDto
+    {
+        public string ID { get; set; }
+        public string Name { get; set; }
+        public string Type { get; set; }
+        public string Price { get; set; }
+    }
+}
--- DVDRentalUI/DVDRental.asmx.cs
+++ DVDRentalUI/DVDRental.asmx.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Services;
-using DVDRentalData;
+using DVDRentalCode;
 
 namespace DVDRentalUI
 {
@@ -14,42 +14,14 @@ namespace DVDRentalUI
     [WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
     [System.ComponentModel.ToolboxItem(false)]
     // To allow this Web Service to be called from script, using ASP.NET AJAX, uncomment the following line. 
-    // [System.Web.Script.Services.ScriptService]
+    [System.Web.Script.Services.ScriptService]
     public class DVDRental : System.Web.Services.WebService
     {
-
-        [WebMethod]
-        public string HelloWorld()
-        {
-            return "Hello World";
-        }
-
         [WebMethod]
         public List<RentalItemDto> GetAllRentalItems()
         {
-            DVDRentalEntities db = new DVDRentalEntities();
-            List<RentalItemDto> LRI = new List<RentalItemDto>();
-            foreach (var ri in db.RentalItems)
-            {
-                LRI.Add(new RentalItemDto
-                {
-                    Name = ri.Name,
-                    Type = ri.Type,
-                    Price = ri.Price.ToString()
-                });
-            }
-            return LRI;
+            var ri = new RentalItemBLL();
+            return ri.GetAllRentalItems();
         }
-
-        #region DTO's
-
-        public class RentalItemDto
-        {
-            public string Name { get; set; }
-            public string Type { get; set; }
-            public string Price { get; set; }
-        }
-
-        #endregion
     }
 }
